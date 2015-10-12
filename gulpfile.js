e841ac77fca57088bdd3767d7e3f87ea80acc809
Sebastien Powell <sebastienpowell@gmail.com>
2015-10-12 22:10:47 +0100

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
 // Load Gulp Dependencies
 var gulp = require("gulp"),
-		sass = require("gulp-ruby-sass");
-		// autoprefixer = require("gulp-autoprefixer"),
+		sass = require("gulp-ruby-sass"),
+		autoprefixer = require("gulp-autoprefixer"),
 		uglify = require("gulp-uglify"),
-		concat = require("gulp-concat"),
-		// notify = require("gulp-notify");
+		concat = require("gulp-concat");
 
 gulp.task('sass', function () {
 	return sass('css/style.scss', { style: 'compact' })
 	.on('error', function (err) {
 		console.error('Error!', err.message);
 	})
+	.pipe(autoprefixer("last 2 version", "> 1%"))
 	.pipe(gulp.dest('css'));
 });
 
 gulp.task("uglify", function() {
-
 	gulp.src(["js/**/*.js"])
 		.pipe(concat("app.js"))
 		.pipe(gulp.dest(""))
 });
-
 
 gulp.task("watch", function() {
 	gulp.watch(['**/*/*.sass', '**/*/*.scss'], ['sass']);
